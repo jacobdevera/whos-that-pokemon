ba4d6c128f69e6024bd5d9d0314b97ebd7fc66e4
Jacob Devera <jacob.devera12@gmail.com>
2016-10-23 22:48:42 -0700

    Added analyze tweets method

--- sentiment/js/app.js
+++ sentiment/js/app.js
@@ -25,14 +25,68 @@ function extractWords(tweet) {
 }
 
 function findSentimentWords(words) {
-	var result = {};
+	var emotions = {};
 	_EMOTIONS.forEach(function(emotion) {
 		var wordsWithEmotion = words.filter(function(word) {
 			if (_SENTIMENTS[word] != undefined) 
 				return _SENTIMENTS[word][emotion] != undefined;
 			return false;
 		});
-		result[emotion] = wordsWithEmotion;
+		emotions[emotion] = wordsWithEmotion;
 	});
-	return result;
+	return emotions;
+}
+
+function analyzeTweets(tweets) {
+	var result = {};
+	/* create words array and word sentiment object as new properties of each tweet */
+	tweets.forEach(function(tweet) {
+		tweet["words"] = extractWords(tweet);
+		tweet["emotions"] = findSentimentWords(tweet["words"]);
+	});
+
+	/* sum words of all tweets */
+	var sumAllWords = tweets.reduce(function(total, tweet) {
+		return total + tweet["words"].length;
+	}, 0);
+
+	/* use list of emotions as new properties of result, initialize word lists, and calculate emotion percentage */
+	_EMOTIONS.forEach(function(emotion) {
+		result[emotion] = {};
+		result[emotion]["words"] = {};
+		var sumEmotion = tweets.reduce(function(total, tweet) {
+			total += tweet["emotions"][emotion].length;
+			return total;
+		}, 0);
+		result[emotion]["percent"] = ((sumEmotion / sumAllWords) * 100) + "%";
+	});
+
+	/* make list of words as a property for each emotion and calculate word frequency */ 
+	tweets.forEach(function(tweet) {
+		_EMOTIONS.forEach(function(emotion) {
+			tweet["emotions"][emotion].forEach(function(word) {
+				if (result[emotion]["words"][word] != undefined) {
+					result[emotion]["words"][word]["count"]++;
+				} else { // initialize count for new words
+					result[emotion]["words"][word] = {};
+					result[emotion]["words"][word]["count"] = 1;
+				}
+			});
+		});
+	});
+	
+	_EMOTIONS.forEach(function(emotion) {
+		var sortedWords = Object.keys(result[emotion]["words"]);
+		sortedWords.sort(function(a, b) {
+			if (result[emotion]["words"][a].count > result[emotion]["words"][b].count) {
+				return -1;
+			} else if (result[emotion]["words"][a].count < result[emotion]["words"][b].count) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		result[emotion]["sortedWords"] = sortedWords;
+		console.log(result);
+	});
 }
