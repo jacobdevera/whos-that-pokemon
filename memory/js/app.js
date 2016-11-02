e2f0bb7d47427a38000e1894c5fc68872c2b1a40
Jacob Devera <jacob.devera12@gmail.com>
2016-11-01 17:59:13 -0700

    Created grid of cards and shuffled them

--- memory/js/app.js
+++ memory/js/app.js
@@ -8,6 +8,8 @@ function createGrid() {
     }
 }
 
+createGrid(); // initialize grid before starting game
+
 $('#start').click(startGame);
 
 function startGame() {
@@ -27,12 +29,23 @@ function shuffleCards() {
     duplicatedCards = _.shuffle(duplicatedCards); // shuffle once more to separate pairs
 
     var board = $('#board');
+    board.empty();
     var row = $(document.createElement('div'));
+    row.attr('role', 'row');
 
     _.forEach(duplicatedCards, function(card, index) {
         if (index % 4 == 0 && index > 0) {
             board.append(row);
             row = $(document.createElement('div'));
+            row.attr('role', 'row');
         }
+        var button = $(document.createElement('button'));
+        button.attr({
+            role: 'gridcell',
+            'aria-label': 'Card' + index
+        });
+        button.addClass('face-down');
+        row.append(button);
     });
+    board.append(row);
 }
