317145d7160b74f9e91ebeb5373467530f6457a7
Jacob Devera <jacob.devera12@gmail.com>
2016-11-01 18:22:50 -0700

    Added accessibility to the grid

--- memory/js/app.js
+++ memory/js/app.js
@@ -34,17 +34,28 @@ function shuffleCards() {
     row.attr('role', 'row');
 
     _.forEach(duplicatedCards, function(card, index) {
+        /* append a new row every four cards */
         if (index % 4 == 0 && index > 0) {
             board.append(row);
             row = $(document.createElement('div'));
             row.attr('role', 'row');
         }
+
+        /* create new face-down card as a button */
         var button = $(document.createElement('button'));
         button.attr({
             role: 'gridcell',
-            'aria-label': 'Card' + index
+            'aria-label': 'Card'
         });
         button.addClass('face-down');
+
+        /* content (card number) that can be read to users for accessibility */
+        var content = $(document.createElement('span'));
+        content.addClass("sr-only sr-only-focusable");
+        content.html(card.cardIndex);
+        content.attr('aria-live', 'polite');
+        button.append(content);
+        
         row.append(button);
     });
     board.append(row);
