09f4c6be723fc0acc05688c7a656706ec29cae6e
Jacob Devera <jacob.devera12@gmail.com>
2016-11-07 13:27:42 -0800

    Initial test fetching of data from API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
+import PokeController from './PokeController';
 
 //a "root" component
 class App extends React.Component {
   //how to display this component
-  render() {
-    return (
-      <div>
-        <h1>Hello React!</h1>
-        <p>If you can see this, then things are working!</p>
-        <p>Modify the <code>src/App.js</code> file to define your components</p>
-      </div>
-    );
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         regions: []
+      };
+   }
+
+   componentDidMount() {
+      /* fetch region data */
+      var regionArray = [];
+      PokeController.fetchData('/region/')
+      .then ( (data) => {
+         data.results.forEach(function(region){ 
+            regionArray.push(<Card className='small' title={region.name} key={region.name} />);
+         })
+         this.setState({regions: regionArray});
+      });
+   }
+
+   render() {
+      return (
+         <div>
+            <h1>Who's that Pokemon?</h1>
+            <RegionCards regions={this.state.regions}/>
+         </div>
+      );
+   }
 }
 
-//more Components can go here!
+import { Card, CardTitle, Col } from 'react-materialize';
 
+class RegionCards extends React.Component {
+   render() {
+      return (
+         <div>
+            {this.props.regions}
+         </div>
+      );
+   }
+}
 
 export default App; //make this class available to other files (e.g., index.js)
