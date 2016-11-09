9377174a5aa3c078dc10792761f7933eee21ae7b
Jacob Devera <jacob.devera12@gmail.com>
2016-11-09 14:37:12 -0800

    Added navigation menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PokeController from './PokeController';
-import { Card, CardTitle, Col, Collapsible, CollapsibleItem } from 'react-materialize';
+import { Navbar, NavItem, Button, Icon, Collapsible, CollapsibleItem } from 'react-materialize';
 import _ from 'lodash';
 
 class App extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         regions: []
+         regions: [],
+         selectedRegion: null
       };
    }
 
@@ -17,20 +18,21 @@ class App extends React.Component {
       PokeController.fetchData('/region/')
       .then ((data) => {
          data.results.forEach(function(region){ 
-            regionArray.push(<RegionItem header={_.capitalize(region.name)} key={(region.name)}> </RegionItem>);
+            regionArray.push(<RegionItem name={_.capitalize(region.name)} key={(region.name)}> </RegionItem>);
          })
          this.setState({regions: regionArray});
-      });
-      $(document).ready(function(){
-         $('.collapsible').collapsible();
-      });
-        
+      });    
    }
 
    render() {
       return (
          <div>
-            <h1>Who's that Pokemon?</h1>
+            <Navbar brand="Pokemon?" right>
+               <NavItem href='index.html#'><Icon>search</Icon></NavItem>
+               <NavItem href='index.html#'><Icon>view_module</Icon></NavItem>
+               <NavItem href='index.html#'><Icon>refresh</Icon></NavItem>
+               <NavItem href='index.html#'><Icon>more_vert</Icon></NavItem>
+            </Navbar>
             <RegionCollection regions={this.state.regions}/>
          </div>
       );
@@ -51,9 +53,13 @@ class RegionCollection extends React.Component {
 }
 
 class RegionItem extends React.Component {
+   setRegion() {
+      console.log('click');
+   }
+
    render() {
       return (
-         <CollapsibleItem header={this.props.header}>Let's play</CollapsibleItem>
+         <CollapsibleItem header={this.props.name} onSelect={this.setRegion.bind(this)}>Let's play</CollapsibleItem>
       );
    }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; //import our component
+import App from './App'; // import our component
 
-//can load other CSS files (e.g,. Bootstrap) here
+// can load other CSS files (e.g,. Bootstrap) here
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import 'whatwg-fetch/fetch.js';
 
-//load our CSS file
+// load our CSS file
 import './index.css';
 
-//render the Application view
+// render the Application view
 ReactDOM.render(
   <App />,
   document.getElementById('root')
